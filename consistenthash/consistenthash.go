0975ec3d7a4f8e6a2faa504c43c29fa7a3c3330d
taofengliu <taofeng_liu@outlook.com>
2021-12-30 15:20:18 +0800

    router感知节点宕机

--- consistenthash/consistenthash.go
+++ consistenthash/consistenthash.go
@@ -12,7 +12,7 @@ type Hash func(data []byte) uint32
 type Map struct {
 	hash     Hash
 	replicas int            //虚拟节点的倍数，代表每个真实节点对应几个虚拟节点
-	keys     []int          //经过排序，代表一个哈希环
+	Keys     []int          //经过排序，代表一个哈希环
 	hashMap  map[int]string //虚拟节点和真实节点的映射，虚拟节点即为一个hash值，真实节点为一个地址
 }
 
@@ -40,29 +40,49 @@ func (m *Map) Add(addrs ...string) {
 		for i := 0; i < m.replicas; i++ {
 			hash := int(m.hash([]byte(strconv.Itoa(i) + addr)))
 
-			//将节点的哈希值加入数组
-			m.keys = append(m.keys, hash)
+			//将节点的哈希值加入切片
+			m.Keys = append(m.Keys, hash)
 
 			//将虚拟节点映射到真实节点地址
 			m.hashMap[hash] = addr
 		}
 
 	}
-	sort.Ints(m.keys) //使数组有序
+	sort.Ints(m.Keys) //使数组有序
+}
+
+// Delete 从Map中删除元素
+func (m *Map) Delete(addrs ...string) {
+	for _, addr := range addrs {
+
+		//删除虚拟节点
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + addr)))
+
+			//从切片中删除虚拟节点的哈希值
+			index := sort.SearchInts(m.Keys, hash)
+			m.Keys = append(m.Keys[:index], m.Keys[index+1:]...)
+
+			//删除map中的映射
+			delete(m.hashMap, hash)
+
+		}
+
+	}
 }
 
 // Get 根据key值来确定真实节点地址
 func (m *Map) Get(key string) string {
-	if len(m.keys) == 0 {
+	if len(m.Keys) == 0 {
 		return ""
 	}
 	hash := int(m.hash([]byte(key)))
 
-	//寻找大于key的哈希值的最小虚拟节点哈希值
-	idx := sort.Search(len(m.keys), func(i int) bool {
-		return m.keys[i] >= hash
+	//二分查找大于key的哈希值的最小虚拟节点哈希值
+	idx := sort.Search(len(m.Keys), func(i int) bool {
+		return m.Keys[i] >= hash
 	})
 
 	//取模是因为key的hash可能大于所有节点的hash，这时应定位到第一个节点
-	return m.hashMap[m.keys[idx%len(m.keys)]]
+	return m.hashMap[m.Keys[idx%len(m.Keys)]]
 }
--- server/router.go
+++ server/router.go
@@ -5,9 +5,11 @@ import (
 	"cache/consistenthash"
 	"cache/lru/linkedlist"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -52,7 +54,7 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		value, statusCode := router.getRoute(key)
-		if statusCode == 404 {
+		if statusCode == 404 || statusCode == 999 {
 
 			//未命中，返回404
 			http.Error(w, "cache miss", 404)
@@ -78,12 +80,25 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		nodeAddr := router.HashCircle.Get(key)
 		value, _ := ioutil.ReadAll(r.Body)
-
-		//复制value的reader
-		reader := bytes.NewReader(value)
-
-		resp, _ := http.Post("http://"+nodeAddr+defaultBasePath+key, "raw", reader)
+		resp, err := http.Post("http://"+nodeAddr+defaultBasePath+key, "raw", bytes.NewReader(value))
 		defer resp.Body.Close()
+
+		//发现节点宕机,删除该节点，并不断尝试设值直到成功将值设到正常的节点中，如果节点全部失效，关闭当前Router
+		for err != nil && len(router.HashCircle.Keys) != 0 {
+			router.Log("Node DOWN: " + nodeAddr)
+			router.HashCircle.Delete(nodeAddr)
+			nodeAddr = router.HashCircle.Get(key)
+			resp, err = doPost("http://"+nodeAddr+defaultBasePath+key, "raw", bytes.NewReader(value))
+		}
+
+		//全部节点均失效
+		if len(router.HashCircle.Keys) == 0 {
+			router.Log("No node running")
+			router.Log("router DOWN")
+			http.Error(w, "router DOWN", http.StatusServiceUnavailable)
+			os.Exit(-1)
+		}
+
 		router.Log("SET " + key + ":" + string(value))
 	}
 }
@@ -91,7 +106,15 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 //查询对应节点
 func (router *Router) getRoute(key string) (*linkedlist.Data, int) {
 	nodeAddr := router.HashCircle.Get(key)
-	resp, _ := http.Get("http://" + nodeAddr + defaultBasePath + key)
+	resp, err := http.Get("http://" + nodeAddr + defaultBasePath + key)
+
+	//发现节点宕机，删除该节点
+	if err != nil {
+		router.Log("Node down: " + nodeAddr)
+		router.HashCircle.Delete(nodeAddr)
+		return nil, 999
+	}
+
 	defer resp.Body.Close()
 	statusCode := resp.StatusCode
 	if statusCode == 404 {
@@ -99,4 +122,11 @@ func (router *Router) getRoute(key string) (*linkedlist.Data, int) {
 	}
 	value, _ := ioutil.ReadAll(resp.Body)
 	return &linkedlist.Data{B: value}, 200
+}
+
+//为了关闭尝试节点是否有效的请求中的所有Body，创建此函数
+func doPost(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
+	resp, err = http.Post(url, contentType, body)
+	defer resp.Body.Close()
+	return resp, err
 }
